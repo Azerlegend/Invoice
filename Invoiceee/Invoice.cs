b5adf0b73dc24f2f117c83b8bbea9364b09b40b3
Azerlegend <azarak@code.edu.az>
2023-01-30 18:33:48 +0400

    Invoice

---
+++ Invoiceee/Invoice.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+namespace Invoice
+{
+
+    public class Invoice
+    {
+
+        private int account;
+        private string customer;
+        private string provider;
+
+        public Invoice(int account, string customer, string provider)
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+        }
+
+        private string article;
+
+        public string Article
+        {
+            get { return article; }
+            set { article = value; }
+        }
+
+        private int quantity;
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+        private decimal price;
+
+        public decimal Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public void CostCalculation(bool NeedEdv)
+        {
+            if (NeedEdv == true)
+            {
+                Console.WriteLine(Price * Quantity * (decimal)0.18);
+            }
+            else
+            {
+                Console.WriteLine(Price * Quantity);
+            }
+        }
+    }
+}
